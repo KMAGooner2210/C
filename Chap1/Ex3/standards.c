5ad8c451718ea10e66194986029e871e84536bc4
KMAGooner2210 <maithanhtung221099@gmail.com>
2026-05-05 00:08:48 +0700

    Chap1 Exercises

---
+++ Chap1/Ex3/standards.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	int unused_var = 10;
+	for(int i = 0; i < 5; i++){
+		printf("Gia tri cua i la: %d\n", i);
+	}
+	return 0;
+}
---
+++ Chap1/Ex4/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "math_ops.h"
+
+int main(){
+	int result = add(5, 7);
+	printf("Ket qua la: %d\n", result);
+	return 0;
+}
